162238f7c384ee665e2fa8973895ebed2aa550c7
kidd <ekidd@qa.com>
2021-01-22 22:08:50 +0000

    inital write up of ItemDao without testing and some Item versions of Customer classes with names changed. Not yet tested

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDao.java
@@ -0,0 +1,118 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DatabaseUtilities;
+
+public class ItemDao implements IDomainDao<Item> {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    public Item create(Item item) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                PreparedStatement statement = connection
+                        .prepareStatement("INSERT INTO Items(first_name, price) VALUES (?, ?)");) {
+            statement.setString(1, item.getName());
+            statement.setDouble(2, item.getPrice());
+            statement.executeUpdate();
+            return readLatest();
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+    public Item read(Long id) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM Items WHERE id = ?");) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            return modelFromResultSet(resultSet);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public List<Item> readAll() {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM Items");) {
+            List<Item> Items = new ArrayList<>();
+            while (resultSet.next()) {
+                Items.add(modelFromResultSet(resultSet));
+            }
+            return Items;
+        } catch (SQLException e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return new ArrayList<>();
+    }
+
+    public Item readLatest() {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM Items ORDER BY id DESC LIMIT 1");) {
+            resultSet.next();
+            return modelFromResultSet(resultSet);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public Item update(Item Item) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                PreparedStatement statement = connection
+                        .prepareStatement("UPDATE Items SET first_name = ?, price = ? WHERE id = ?");) {
+            statement.setString(1, Item.getName());
+            statement.setDouble(2, Item.getPrice());
+            statement.setLong(3, Item.getId());
+            statement.executeUpdate();
+            return read(Item.getId());
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public int delete(long id) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                Statement statement = connection.createStatement();) {
+            return statement.executeUpdate("delete from Items where id = " + id);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return 0;
+    }
+
+    @Override
+    public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getLong("id");
+        String name = resultSet.getString("first_name");
+        double price = resultSet.getDouble("price");
+        return new Item(id, name, price);
+    }
+
+}
