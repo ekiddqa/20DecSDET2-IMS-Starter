a2fd96a6753d52a1100bb3e802c0cd98ab75f555
kidd <ekidd@qa.com>
2021-01-23 20:57:26 +0000

    Fully copied all customer CRUD functions into Items. Human tests have not been performed nor have any tests been written.

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -5,14 +5,12 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.qa.ims.persistence.dao.IDomainDao;
 import com.qa.ims.persistence.dao.ItemDao;
 import com.qa.ims.persistence.domain.Item;
-import com.qa.ims.persistence.domain.item;
 import com.qa.ims.utils.JavaUtilities;
 
 
-public class ItemController implements IDomainDao<Item>{
+public class ItemController implements ICrudController<Item> {
 	//yelling because CRUD methods aren't final since they need to convert to doubles
 
 	    public static final Logger LOGGER = LogManager.getLogger();
@@ -34,41 +32,51 @@ public class ItemController implements IDomainDao<Item>{
 	        }
 	        return items;
 	    }
+	    
+	 
+	 	@Override
+		public Item create() {
+	 			 LOGGER.info("Please enter a product name");
+	 	        String name = javaUtilities.getString();
+	 	        LOGGER.info("Please enter the price");
+	 	        String p = javaUtilities.getString();
+	 	        try {
+	 	        	double price = Double.parseDouble(p);
+	 	        	   Item item = itemDao.create(new Item(name, price));
+	 	   	        LOGGER.info("item created");
+	 	   	        return item;
+	 	        } catch(Exception e) {
+	 	        	LOGGER.info("Invalid entry, price must be a number.");
+	 	        } return null;
+	 		}
+	 	    
 
-	    @Override
-	    public Item create() {
-	        LOGGER.info("Please enter a product name");
-	        String name = javaUtilities.getString();
-	        LOGGER.info("Please enter the price");
-	        String price = javaUtilities.getString();
-	        LOGGER.info("Please enter the price");
-	        String  = javaUtilities.getString();
-	        //convert to double -will need to include try catch
-	        Item item = itemDao.create(new Item(name, price));
-	        LOGGER.info("item created");
-	        return item;
-	    }
+		@Override
+		public Item update() {
+			  LOGGER.info("Please enter the id of the item you would like to update");
+		        Long id = javaUtilities.getLong();
+		        LOGGER.info("Please enter a first name");
+		        String name = javaUtilities.getString();
+		        LOGGER.info("Please enter a price");
+		        String p = javaUtilities.getString();
+		        try {
+		        	double price = Double.parseDouble(p);
+		        	//convert to double
+			        Item item = itemDao.update(new Item(id, name, price));
+			        LOGGER.info("item Updated");
+			        return item;
+		        } catch (Exception e){
+		        	LOGGER.info("Invalid entry, price must be a number.");
+		        }
+			return null;
+		}
 
-	    @Override
-	    public Item update() {
-	        LOGGER.info("Please enter the id of the item you would like to update");
-	        Long id = javaUtilities.getLong();
-	        LOGGER.info("Please enter a first name");
-	        String name = javaUtilities.getString();
-	        LOGGER.info("Please enter a price");
-	        String price = javaUtilities.getString();
-	        //convert to double
-	        Item item = itemDao.update(new Item(id, name, price));
-	        LOGGER.info("item Updated");
-	        return item;
-	    }
-
-	    @Override
-	    public int delete() {
-	        LOGGER.info("Please enter the id of the item you would like to delete");
+		@Override
+		public int delete() {
+			LOGGER.info("Please enter the id of the item you would like to delete");
 	        Long id = javaUtilities.getLong();
 	        return itemDao.delete(id);
-	    }
+		}
 
 }
 
