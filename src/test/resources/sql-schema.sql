888f0eebe157472e28822e9eb0cd4074c379af89
kidd <ekidd@qa.com>
2021-01-25 13:38:15 +0000

    ostly works in console but having issues with database - no actual changes to items AFAIK

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,3 +6,30 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    `name` VARCHAR(40) NOT NULL,
+    `price` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    `fk_customer_id` int(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`fk_customer_id`) REFERENCES `ims`.`customers`(`id`)
+);
+
+DROP TABLE IF EXISTS `order_time`;
+
+CREATE TABLE IF NOT EXISTS `ims`.`order_item` (
+    `fk_order_id` INT(11) NOT NULL,
+    `fk_item_id` int(11) NOT NULL,
+    FOREIGN KEY (`fk_order_id`) REFERENCES `ims`.`orders`(`id`),
+    FOREIGN KEY (`fk_item_id`) REFERENCES `ims`.`items`(`id`)
+);
