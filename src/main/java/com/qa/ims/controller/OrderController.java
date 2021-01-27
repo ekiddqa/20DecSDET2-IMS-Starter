114f10a4ff32f785fccf83f0e3642a0d005b33f2
kidd <ekidd@qa.com>
2021-01-27 15:50:07 +0000

    Making progress on Order CRUD. Object class untested but complete for now. Same with Controller. DAO still has errors to fix.

---
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -0,0 +1,102 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.CustomerDao;
+import com.qa.ims.persistence.dao.OrderDao;
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.JavaUtilities;
+
+public class OrderController implements ICrudController<Order> {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    private OrderDao orderDao;
+	CustomerDao customerDao = new CustomerDao();
+    private JavaUtilities javaUtilities;
+    
+
+    public OrderController(OrderDao orderDao, JavaUtilities javaUtilities) {
+        super();
+        this.orderDao = orderDao;
+        this.javaUtilities = javaUtilities;
+    }
+
+    @Override
+    public List<Order> readAll() {
+        List<Order> orders = orderDao.readAll();
+       if(orders.isEmpty()) {
+    	   LOGGER.info("No orders in IMS.");
+       } 
+       for (Order order : orders) {
+            LOGGER.info(order);
+        }
+        return orders;
+    }
+    
+ 
+	/*
+	 * @Override public Order create() { boolean addMore = true;
+	 * LOGGER.info("Please enter a customer ID"); try { String c =
+	 * javaUtilities.getString(); Long customerId = Long.parseLong(c); Customer
+	 * customer = customerId. List<Item> itemList = new ArrayList<Item>();
+	 * //seperate method for creating an empty order and other for adding items to
+	 * it do { LOGGER.info("Please enter item ID. Enter 0 to exit."); String i =
+	 * javaUtilities.getString(); if(i != "0") { Long itemId = Long.parseLong(i);
+	 * itemList.add(itemId); } addMore = false; } while(addMore); Order order =
+	 * orderDao.create(new Order(customer, itemList)); LOGGER.info("order created");
+	 * return order; } catch(Exception e) { LOGGER.
+	 * info("Invalid entry, customer and product ID must be positive integers."); }
+	 * return null; }
+	 */
+ 	    
+    @Override
+    
+    public Order create() {
+    	LOGGER.info("Please enter a customer ID."); //
+    	try {
+    		Long customerId = javaUtilities.getLong();
+    		Order order = orderDao.create(new Order(new Customer(customerId, null, null), 0.0));
+   	        return order; 
+    	} catch (Exception e) {
+    		LOGGER.info("Invalid input.");
+    		return null;
+    	}
+    }
+    
+	@Override
+	public Order update() { //updated order as null
+		//ask for order id
+		//add or remove - conditional (if/elif(/else - invalid selection))
+		Order nullOrder = null;  
+		LOGGER.info("Please enter the id of the order you would like to update"); //add/remove item functionality here
+	        Long id = javaUtilities.getLong();
+	        LOGGER.info("Would you like to add or remove an item?");
+	        String input = javaUtilities.getString();
+	        if(input.equals("add")) {
+	        	LOGGER.info("Enter ID of item to add to order.");
+	        	Long toAdd = javaUtilities.getLong();
+	        	nullOrder = this.orderDao.addItems(id, toAdd);
+	        } else if(input.equals("remove")) {
+	            LOGGER.info("Enter ID of item to remove from order.");
+	            Long toRemove = javaUtilities.getLong();
+	            nullOrder = this.orderDao.deleteItem(id, toRemove);
+	        }	else {
+	        	LOGGER.info("Invalid input. Only \"add\" or \"remove\" are accepted.");
+	        }
+	        	return nullOrder;
+	        	
+	}
+
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the item you would like to delete");
+        Long id = javaUtilities.getLong();
+        return orderDao.delete(id);
+	}
+    
+}
---
+++ src/main/java/com/qa/ims/persistence/dao/OrderDao.java
@@ -0,0 +1,169 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DatabaseUtilities;
+
+public class OrderDao implements IDomainDao<Order> {
+
+	    public static final Logger LOGGER = LogManager.getLogger();
+	    private ItemDao itemDao;
+	    private CustomerDao customerDao;
+	    
+	    public OrderDao(ItemDao itemDao, CustomerDao customerDao) {
+	    	super();
+	    	this.itemDao = itemDao;
+	    	this.customerDao = customerDao;
+	    }
+
+	    @Override
+	    public Order create(Order order) {
+	        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+	                PreparedStatement statementOrder = connection
+	                        .prepareStatement("INSERT INTO orders(fk_customer_id) VALUE (?)");) {
+	        	statementOrder.setLong(1, order.getCustomer().getId());
+	            statementOrder.executeUpdate(); 
+	            return readLatest();
+	        } catch (Exception e) {
+	            LOGGER.debug(e);
+	            LOGGER.error(e.getMessage());
+	        }
+	        return null;
+	    }
+
+	    public Order read(Long id) {
+	        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+	                PreparedStatement statement = connection.prepareStatement("SELECT orders.id as order_id, first_name.customers, "
+	                		+ "surname.customers, name.item FROM orders, JOIN orders on order_item.fk_order_id=order.id, "
+	                		+ "JOIN order_item on items.id = order_item.fk_item_id WHERE fk_order_id = ?");) {
+	            statement.setLong(1, id);
+	            ResultSet resultSet = statement.executeQuery();
+	            resultSet.next();
+	            return modelFromResultSet(resultSet);
+	        } catch (Exception e) {
+	            LOGGER.debug(e);
+	            LOGGER.error(e.getMessage());
+	        }
+	        return null;
+	    }
+
+	    @Override
+	    public List<Order> readAll() {
+	        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+	                Statement statement = connection.createStatement();
+	                ResultSet resultSet = statement.executeQuery("SELECT orders.id as order_id, first_name.customers, surname.customers,"
+	                		+ " name.item FROM orders, JOIN orders on order_item.fk_order_id=order.id,"
+	                		+ " JOIN order_item on items.id = order_item.fk_item_id;");) {
+	            List<Order> orders = new ArrayList<>();
+	            while (resultSet.next()) {
+	                orders.add(modelFromResultSet(resultSet));
+	            }
+	            return orders;
+	        } catch (SQLException e) {
+	            LOGGER.debug(e);
+	            LOGGER.error(e.getMessage());
+	        }
+	        return new ArrayList<>();
+	    }
+
+	    public Order readLatest() {
+	        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+	                Statement statement = connection.createStatement();
+	                ResultSet resultSet = statement.executeQuery("SELECT orders.id as order_id, first_name.customers, surname.customers,"
+	                		+ " name.item FROM orders," + " JOIN orders on order_item.fk_order_id=order.id,"
+	                		+ " JOIN order_item on items.id = order_item.fk_item_id ORDER BY id DESC LIMIT 1");) {
+	            resultSet.next();
+	            return modelFromResultSet(resultSet);
+	        } catch (Exception e) {
+	            LOGGER.debug(e);
+	            LOGGER.error(e.getMessage());
+	        }
+	        return null;
+	    }
+    
+	    public Item getItems(Long id) {
+			return itemDao.read(id);
+	    	
+	    }
+	    public Order addItems(Long orderId, Long itemId) {
+	    	   try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+		                PreparedStatement statement = connection
+		                        .prepareStatement("INSERT INTO orders_item (fk_order_id, fk_item_id) VALUE (?,)");) {
+		        	statement.setLong(1, orderId);
+		        	statement.setLong(2, itemId);
+		            statement.executeUpdate(); 
+		            return readLatest();
+		        } catch (Exception e) {
+		            LOGGER.debug(e);
+		            LOGGER.error(e.getMessage());
+		        }
+		        return null;
+		    }
+	    
+	    public double sumValue(Long id) {
+	    	double sum = 0;
+	    	order.getItem.stream().forEach(item -> sum += sum + item.getPrice());
+	    	return sum;
+	    }
+	    
+	    @Override
+	    public Order update(Order order) {
+	        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+	                PreparedStatement statement = connection
+	                        .prepareStatement("UPDATE order SET customer = ?, WHERE id = ?, UPDATE order_item SET item");) {
+	            order.getItem().stream().forEach(x -> statement.setLong(1, order.getCustomer().getId()),
+	            statement.setLong(2, order.getItem().),
+	            statement.setLong(3, order.getId()));
+	            statement.executeUpdate();
+	            return read(order.getId());
+	        } catch (Exception e) {
+	            LOGGER.debug(e);
+	            LOGGER.error(e.getMessage());
+	        }
+	        return null;
+	    }
+
+	    @Override
+	    public int delete(long id) { //delete an entire order
+	        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+	                Statement statement = connection.createStatement();) {
+	            return statement.executeUpdate("delete from orders where id = " + id + ", delete from order_items where fk_order_id = " + id);
+	        } catch (Exception e) {
+	            LOGGER.debug(e);
+	            LOGGER.error(e.getMessage());
+	        } return 0;
+	    }
+	        
+	     public Order deleteItem(long id, long orderId) { //delete an item from an order
+		      try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+		              Statement statement = connection.createStatement();) {
+		         statement.executeUpdate("delete from orders_items where fk_item_id = " + id + " AND  fk_order_id = " + orderId);
+		     } catch (Exception e) {
+		          LOGGER.debug(e);
+		          LOGGER.error(e.getMessage());
+		     } return read(id);
+	    }
+
+	    @Override
+	    public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
+	        Long id = resultSet.getLong("id");
+	        Customer customer = customerDao.read(resultSet.getLong("fk_customer_id")); //init customerId
+	        List<Item> item = getItem();//need to make a get items only from order method
+	        return new Order(id, customer, item);
+	    }
+
+}
