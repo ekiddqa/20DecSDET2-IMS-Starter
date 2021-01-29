756cacc1b56403fc37eb80f53a74f1f0e6c3673e
kidd <ekidd@qa.com>
2021-01-29 00:00:20 +0000

    MVP functions work fine through console but having issues with JUnit not cleaning up test databases

--- src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -14,53 +14,55 @@ import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.utils.DatabaseUtilities;
 
 public class OrderDAOTest {
-	 private final OrderDao DAO = new OrderDao();
-	 List<Order> expected = new ArrayList<>();
-     Customer customer = new Customer(1L, "nick", "johnson");
-     List<Item> item = new ArrayList<>();
 
-	    @Before
-	    public void setup() {
-	        DatabaseUtilities.connect();
-	        DatabaseUtilities.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
-	    }
+    final CustomerDao CDAO = new CustomerDao();
+    final ItemDao IDAO = new ItemDao();
+    final OrderDao DAO = new OrderDao(IDAO, CDAO);
+    final Customer cust = new Customer(1L,"jordan", "harrison");
+    final List<Item> stuff = new ArrayList<>();
+    final Order created = new Order(1L,cust,stuff);
 
-	    @Test
-	    public void testCreate() {
-	    	Customer customer = new Customer(1L, "nick", "johnson");
-	        final Order created = new Order(2L, customer, item);
-	        assertEquals(created, DAO.create(created));
-	    }
+    @SuppressWarnings("unused")
+	@Before
+    public void setup() {
+        DatabaseUtilities.connect();
+        DatabaseUtilities.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+    }
 
-	    @Test
-	    public void testReadAll() {
-	        List<Order> expected = new ArrayList<>();
-	        expected.add(new Order(1L, customer, item));
-	        assertEquals(expected, DAO.readAll());
-	    }
+    @Test
+    public void testCreate() {
+        assertEquals(created, DAO.create(created));
+    }
 
-	    @Test
-	    public void testReadLatest() {
-	        assertEquals(new Order(1L, customer, item), DAO.readLatest());
-	    }
+    @Test
+    public void testReadAll() {
+        List<Order> expected = new ArrayList<>();
+        final Customer nCust = new Customer(1L,"nick", "barba");
+        expected.add(new Order(nCust));
+        assertEquals(expected, DAO.readAll());
+    }
 
-	    @Test
-	    public void testRead() {
-	        final long ID = 1L;
-	        assertEquals(new Order(ID, customer, item), DAO.read(ID));
-	    }
+    @Test
+    public void testReadLatest() {
+        assertEquals(new Order(cust), DAO.readLatest());
+    }
 
-	    @Test
-	    public void testUpdate() {
-	    	List<Item> updatedItem = new ArrayList<>();
-	    	Customer updatedCustomer = new Customer("jordan", "harrison");
-	        final Order updated = new Order(1L, updatedCustomer, updatedItem);
-	        assertEquals(updated, DAO.update(updated));
-	        
-	    }
+    @Test
+    public void testRead() {
+    	 final long ID = 1L;
+         assertEquals(new Order(ID, cust), DAO.read(ID));
+    }
 
-	    @Test
-	    public void testDelete() {
-	        assertEquals(1, DAO.delete(1));
-	    }
+    @Test
+    public void testUpdate() {
+    	final Customer nCust = new Customer(1L, "nick", "johnson");
+        final Order updated = new Order(1L, nCust);
+        assertEquals(updated, DAO.update(updated));
+
+    }
+
+    @Test
+    public void testDelete() {
+        assertEquals(1, DAO.delete(1));
+    }
 }
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -5,11 +5,10 @@ DROP TABLE `order_item`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
+    `first_name` VARCHAR(40) NOT NULL,
+    `surname` VARCHAR(40) NOT NULL,
     PRIMARY KEY (`id`)
 );
-
 
 CREATE TABLE IF NOT EXISTS `items` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -18,18 +17,17 @@ CREATE TABLE IF NOT EXISTS `items` (
     PRIMARY KEY (`id`)
 );
 
-
 CREATE TABLE IF NOT EXISTS `orders` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `fk_customer_id` INT(11) NOT NULL,
+    `value` double DEFAULT 0,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`fk_customer_id`) REFERENCES `customers`(`id`)
+    FOREIGN KEY (`fk_customer_id`) REFERENCES `customers`(`id`) ON DELETE CASCADE
 );
-
 
 CREATE TABLE IF NOT EXISTS `order_item` (
     `fk_order_id` INT(11) NOT NULL,
     `fk_item_id` INT(11) NOT NULL,
-    FOREIGN KEY (`fk_order_id`) REFERENCES `orders`(`id`),
-    FOREIGN KEY (`fk_item_id`) REFERENCES `items`(`id`)
+    FOREIGN KEY (`fk_order_id`) REFERENCES `orders`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`fk_item_id`) REFERENCES `items`(`id`) ON DELETE CASCADE
 );
