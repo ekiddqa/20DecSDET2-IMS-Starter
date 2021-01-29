26380578cfb54b26f0c3baa3d92a95ab5c26f0db
kidd <ekidd@qa.com>
2021-01-29 12:14:00 +0000

    More tests fixed. testRead and testUpdate remain

--- src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -21,51 +21,51 @@ public class OrderDAOTest {
 	private final ItemDao iDAO = new ItemDao();
 	private final OrderDao DAO = new OrderDao(iDAO, cDAO);
 	private Order testOrder;
+	private Order testOrderStuff;
     private Item junk;
     private List<Item> pile;
+    private List<Item> pileEmpty;
+    private Customer customer;
 	
 	@Before
 	public void setup() {
     DatabaseUtilities.connect();
     DatabaseUtilities.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
     final List<Item> pile = new ArrayList<>();
+    final List<Item> pileEmpty = new ArrayList<>();
     Item junk = new Item(1L, "jordan", 50);
+    pile.add(junk);
     final Customer customer = new Customer(1L, "jordan", "harrison");
-    testOrder = new Order(2L, customer, pile, 0.0);
+    testOrder = new Order(2L, customer, pileEmpty);
+    testOrderStuff = new Order(1L, customer, pile, 0.0);
 
 	}
 	
     @Test
     public void testCreate() {
-    	final Order created = testOrder;
-    	assertEquals(created, DAO.create(created));
+    	assertEquals(testOrder, DAO.create(testOrder));
     }
 
     @Test
     public void testReadAll() {
-
         final List<Order> expected = new ArrayList<>();
-        pile.add(junk);
-        final Order created = new Order(1L, new Customer(1L, "jordan", "harrison"), pile);
-        expected.add(created);
+        expected.add(testOrderStuff);
         assertEquals(expected, DAO.readAll());
     }
     
     @Test
     public void testReadLatest() {
-        assertEquals(testOrder, DAO.readLatest());
+        assertEquals(testOrderStuff, DAO.readLatest());
     }
 
     @Test
     public void testRead() {
-    	List<Item> junk = new ArrayList<>();
-    	final Order order = new Order(1L, new Customer("jordan", "harrison"), junk, 0.0);
-        assertEquals(order, DAO.read(1L));
+    	final Long ID = 2L;
+        assertEquals(testOrder, DAO.read(ID));
     }
 
     @Test
     public void testUpdate() {
-    	
     	final List<Item> junk = new ArrayList<>();
         final Order updated = new Order(1L, new Customer(1L, "jordan", "harrsion"), junk, 0.0);
         assertEquals(updated, DAO.update(updated));
